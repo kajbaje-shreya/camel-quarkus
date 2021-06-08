7a523a98f349612885cc3dec3002c2d58b84db0b
Peter Palaga <ppalaga@redhat.com>
2021-06-08 10:27:08 +0200

    ENTESB-17063 AWS2 ddb-streams integration tests failures

--- integration-tests-aws2/aws2-ddb/src/test/java/org/apache/camel/quarkus/component/aws2/ddb/it/Aws2DdbTest.java
+++ integration-tests-aws2/aws2-ddb/src/test/java/org/apache/camel/quarkus/component/aws2/ddb/it/Aws2DdbTest.java
@@ -16,8 +16,11 @@
  */
 package org.apache.camel.quarkus.component.aws2.ddb.it;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
@@ -45,6 +48,42 @@ class Aws2DdbTest {
 
         final String key = "key" + UUID.randomUUID().toString().replace("-", "");
         final String msg = "val" + UUID.randomUUID().toString().replace("-", "");
+        final String initKeyPrefix = "initKey";
+        final String initMsg = "val";
+
+        /* Wait for the consumer to start. Test event has to be created periodically to ensure consumer reception */
+        RestAssured.given()
+                .contentType(ContentType.TEXT)
+                .body("")
+                .post("/aws2-ddb/item/" + initKeyPrefix + UUID.randomUUID().toString().replace("-", ""))
+                .then()
+                .statusCode(201);
+
+        /* Periodically check that init event is received and if nothing is received, invoke another one  */
+        Awaitility.await().pollInterval(1, TimeUnit.SECONDS).atMost(120, TimeUnit.SECONDS).until(
+                () -> {
+                    ExtractableResponse<Response> result = RestAssured.get("/aws2-ddbstream/change")
+                            .then()
+                            .statusCode(200)
+                            .extract();
+
+                    LOG.info("Expecting at least 1 init event, got " + result.statusCode() + ": " + result.body().asString());
+
+                    List<Map> res = result.jsonPath().getList("$", Map.class);
+
+                    /* If there is no received event, consumer is still not running. Repeat insert. */
+                    if (res.isEmpty()) {
+                        RestAssured.given()
+                                .contentType(ContentType.TEXT)
+                                .body(initMsg)
+                                .post("/aws2-ddb/item/initKey" + UUID.randomUUID().toString().replace("-", ""))
+                                .then()
+                                .statusCode(201);
+                    }
+                    return res;
+                },
+                /* If at least one of the init events is recceived, consumer is working */
+                list -> !list.isEmpty());
 
         /* Ensure initially empty */
         RestAssured.get("/aws2-ddb/item/" + key)
@@ -104,23 +143,31 @@ class Aws2DdbTest {
                 },
                 Matchers.is(204));
 
-        /* The above actions should trigger the following three change events */
-        RestAssured.get("/aws2-ddbstream/change")
-                .then()
-                .statusCode(200)
-                .body("key", Matchers.is(key))
-                .body("new", Matchers.is(msg));
-        RestAssured.get("/aws2-ddbstream/change")
-                .then()
-                .statusCode(200)
-                .body("key", Matchers.is(key))
-                .body("old", Matchers.is(msg))
-                .body("new", Matchers.is(newMsg));
-        RestAssured.get("/aws2-ddbstream/change")
-                .then()
-                .statusCode(200)
-                .body("key", Matchers.is(key))
-                .body("old", Matchers.is(newMsg));
+        Awaitility.await().atMost(120, TimeUnit.SECONDS).until(
+                () -> {
+                    ExtractableResponse<Response> result = RestAssured.get("/aws2-ddbstream/change")
+                            .then()
+                            .statusCode(200)
+                            .extract();
+
+                    LOG.info("Expecting 3 events got " + result.statusCode() + ": " + result.body().asString());
+                    List<Map> retVal = result.jsonPath().getList("$", Map.class);
+                    //remove init events
+                    return retVal.stream().filter(m -> !String.valueOf(m.get("key")).startsWith("initKey"))
+                            .collect(Collectors.toList());
+                },
+                /* The above actions should trigger the following three change events (initEvent is also present) */
+                list -> list.size() == 3
+
+                        && key.equals(list.get(0).get("key"))
+                        && msg.equals(list.get(0).get("new"))
+
+                        && key.equals(list.get(1).get("key"))
+                        && msg.equals(list.get(1).get("old"))
+                        && newMsg.equals(list.get(1).get("new"))
+
+                        && key.equals(list.get(2).get("key"))
+                        && newMsg.equals(list.get(2).get("old")));
 
     }
 
